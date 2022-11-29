1425cf882469c98eb1efb4274e6fbee51e9fb851
MBLVRD <badmanskiph@gmail.com>
2022-11-29 13:57:52 +0300

    Тоже можно скипать, чисто для понимания упражнение

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,18 +5,18 @@ import csv
 
 def portfolio_cost(filename):
     total_cost = 0.0
-
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         for rownum, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
             try:
-                shares = int(row[1])
-                price = float(row[2])
-                total_cost += shares * price
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += nshares * price
             except ValueError:
                 print(f'Row {rownum}: Bad row: {row}')
         return total_cost
-
-cost = portfolio_cost('Data/missing.csv')
+    
+cost = portfolio_cost('Data/portfoliodate.csv')
 print('Total cost:', cost)
